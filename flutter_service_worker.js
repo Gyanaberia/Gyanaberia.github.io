10d592c80bfa120759c9ce8430a91db06eb2f2ef
Gyanaberia <gyanaprakashberia@gmail.com>
2023-11-28 16:37:15 +0530

    added second page and routes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "52fc34a70b225832e47b13c384bc9b3e",
-"assets/NOTICES": "7ea7b7f0eada215645a1c17b89fc7bd9",
+"assets/NOTICES": "d38f191853ddcb40ecb9795e63799a57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
 "canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "349f5b22a202b2970fa9149cbcc633ae",
-"/": "349f5b22a202b2970fa9149cbcc633ae",
-"main.dart.js": "1d514efb0195e8d37dd9d9b46c2a7eb4",
+"index.html": "592ceecffe064532c36a9787aa311d1c",
+"/": "592ceecffe064532c36a9787aa311d1c",
+"main.dart.js": "e4c7cd3880411b6bd0fe065ef9c591f3",
 "manifest.json": "20c1250f8cd8609791699490414d7476",
 "version.json": "453e2653da2ec05cae07d9d375d71455"};
 // The application shell files that are downloaded before a service worker can
